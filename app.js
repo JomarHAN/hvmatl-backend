f1ff76c6768ba933d0ec7ddce6093c173ad1d01a
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-05-03 17:20:59 -0400

    initial

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 
 //Setup db connection
-const dbURL = process.env.MONGODB_URL;
-// const dbURL = "mongodb+srv://hvmatl:hvmatl@hvmatl-wqthh.gcp.mongodb.net/hvmatl?retryWrites=true&w=majority";
+// const dbURL = process.env.MONGODB_URL;
+const dbURL = "mongodb+srv://hvmatl:hvmatl@hvmatl-wqthh.gcp.mongodb.net/hvmatl?retryWrites=true&w=majority";
 
-mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log('Connect to MongoDB successfully'), (e) => console.log(e));
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connect to MongoDB successfully'), (e) => console.log(e));
 //Config routes
 const usersRouter = require('./routes/user');
 const authRouter = require('./routes/auth');
@@ -39,19 +39,19 @@ app.use('/file', sftp);
 app.use('/prayerRequest', prayerReq);
 app.use('/allSoulsFeast', allSoulsFeastRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send({message: err.message});
+  res.send({ message: err.message });
 });
 
 module.exports = app;
